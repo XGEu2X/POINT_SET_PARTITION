fd23d3b6ccec55f5de85c250409750da2452ebdc
Gerardo Maldonado <23germail@gmail.com>
2020-02-24 13:01:37 -0600

    Some parameters changed.

--- auto_search/RunAll.sh
+++ auto_search/RunAll.sh
@@ -7,8 +7,9 @@ for ((i=0; i<${#ins[@]}; i++)); do
 	filename=${ins[$i]}
 	filename=${filename:4:-3}
 	(./../build/AutoSearch < 'ins/'$filename'.in') >> 'outs/'$filename'.out';
-	if(($i%5==0))
+	if((($i-1)%5==0))
 	then
 		sleep 300
 	fi
+	echo $filename'.in finished'
 done
